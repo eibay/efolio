7decdbce89378836fa1f0ca47de1cefc05b08e88
Elmer Ibayan <eibayan@gmail.com>
2017-08-23 12:03:17 +1000

    added profile section

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
   // $('.carousel').carousel('dist', 50);
    $('.carousel.carousel-slider').carousel({fullWidth: true});
+
+  $("#nextProfile" ).click(function() {
+    $('.carousel').carousel('next');
+    $('.carousel').carousel('next', 1);
+  });
+
+  $("#prevProfile" ).click(function() {
+    $('.carousel').carousel('prev');
+    $('.carousel').carousel('prev', 1);
+  });
 });
+
+
+
         
